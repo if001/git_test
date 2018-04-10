c8f777bd1daca82db33b3afccf6493137c762602
Unknown <otomijuf.004@gmail.com>
2018-04-10 22:40:07 +0900

    修正

--- test.py
+++ test.py
@@ -31,9 +31,9 @@ class MarkDown():
 
 class Report():
     def print_head(self, head):
-        print("-- head -----------------------")
+        print("-- head ---------")
         print(head)
-        print("-- head -----------------------")
+        print("-- head ---------")
 
     def print_body(self, body):
         print("== body =========")
