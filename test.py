ce26eeb761038d06b55631034746e59e725468a3
Unknown <otomijuf.004@gmail.com>
2018-04-10 21:41:28 +0900

    バグ修正

--- test.py
+++ test.py
@@ -1,28 +1,32 @@
 class Document():
-    def init(self, printer):
+    def __init__(self, printer):
         self.printer = printer()
-        
+
     def print_doc(self):
         self.printer.print_head()
         self.printer.print_body()
 
 class Html():
-    def print_head():
+    def print_head(self):
         print("<head></head>")
-    def print_body():
+
+    def print_body(self):
         print("<body></body>")
 
 
 class MarkDown():
-    def print_head():
+    def print_head(self):
         print("# head")
-    def print_body():
+
+    def print_body(self):
         print("# body")
 
 def main():
+    md = Document(MarkDown)
+    md.print_doc()
+
     html = Document(Html)
     html.print_doc()
-
 
 if __name__ == '__main__':
     main()
