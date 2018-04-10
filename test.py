189dcfd1df5a58b3eeaf784e7eecfb0d257e39fc
Unknown <otomijuf.004@gmail.com>
2018-04-10 21:58:42 +0900

    set head body

--- test.py
+++ test.py
@@ -1,32 +1,41 @@
 class Document():
-    def __init__(self, printer):
+    def __init__(self, printer, head):
+        self.head = head
         self.printer = printer()
 
-    def print_doc(self):
-        self.printer.print_head()
-        self.printer.print_body()
+    def print_doc(self, body):
+        self.printer.print_head(self.head)
+        self.printer.print_body(body)
 
 class Html():
-    def print_head(self):
-        print("<head></head>")
+    def print_head(self, head):
+        print("<head>")
+        print(head)
+        print("</head>")
 
-    def print_body(self):
-        print("<body></body>")
 
+    def print_body(self, body):
+        print("<body>")
+        print(body)
+        print("</body>")
 
 class MarkDown():
-    def print_head(self):
-        print("# head")
+    def print_head(self, head):
+        print("<head>")
+        print(head)
+        print("</head>")
 
-    def print_body(self):
-        print("# body")
+    def print_body(self, body):
+        print("<body>")
+        print(body)
+        print("</body>")
 
 def main():
-    md = Document(MarkDown)
-    md.print_doc()
-
-    html = Document(Html)
-    html.print_doc()
+    md = Document(MarkDown, "md")
+    md.print_doc("よい天気ですね!")
+    print("-----------------")
+    html = Document(Html, "html")
+    html.print_doc("よい天気ですね!")
 
 if __name__ == '__main__':
     main()
